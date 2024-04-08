5a634f579ed9221fc58289cc7c653371e8f5b83e
dpfls0922 <dpfls0922@naver.com>
2024-04-08 11:51:15 +0900

    [Feat] 뷰 템플릿 컨트롤러 구현

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -1,2 +1,30 @@
-package hello.springmvc.basic.response;public class ResponseViewController {
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    // 권장하지 않는 방법
+    @RequestMapping("/response/hello") // 경로 이름이랑 뷰 이름이랑 같아서 이렇게 쓸 수 있음
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
 }
